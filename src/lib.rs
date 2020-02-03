5439b9d1011d98a3f27f57830fc2f7f305e6cc2b
Jackson Owens <jackson_owens@alumni.brown.edu>
2020-02-03 16:03:43 -0500

    fix comment for color histogram

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ extern crate image;
 
 use std::collections::HashMap;
 
-/// entropy calculates the entropy of an image's intensities.
+/// entropy calculates the entropy of an image.
 pub fn entropy(im: image::DynamicImage) -> f64 {
     let rgb_im = im.to_rgb();
 
-    // Calculate the histogram of grayscale intensities.
+    // Calculate the histogram of (r,g,b) tuples.
     let mut hist = HashMap::<image::Rgb<u8>, u64>::new();
     for pixel in rgb_im.pixels() {
         let count = hist.entry(*pixel).or_default();
