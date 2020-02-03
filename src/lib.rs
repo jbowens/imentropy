e7b73f507a2c55674a28ede6592dfa93e274d806
Jackson Owens <jackson_owens@alumni.brown.edu>
2020-02-03 13:36:25 -0500

    don't convert to grayscale

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,17 @@ extern crate image;
 use std::collections::HashMap;
 
 /// entropy calculates the entropy of an image's intensities.
-/// The provided image is converted to grayscale, a histogram
-/// of the grayscale values is constructed, and the
-/// formula -sum(p*log2(p)) is calculated.
 pub fn entropy(im: image::DynamicImage) -> f64 {
-    let gray = image::imageops::colorops::grayscale(&im);
+    let rgb_im = im.to_rgb();
 
     // Calculate the histogram of grayscale intensities.
-    let mut hist = HashMap::<u8, u64>::new();
-    for pixel in gray.pixels() {
-        let x : u8 = pixel[0];
-        let count = hist.entry(x).or_default();
+    let mut hist = HashMap::<image::Rgb<u8>, u64>::new();
+    for pixel in rgb_im.pixels() {
+        let count = hist.entry(*pixel).or_default();
         *count += 1;
     }
 
-    let pixel_count = (gray.width() * gray.height()) as f64;
+    let pixel_count = (rgb_im.width() * rgb_im.height()) as f64;
     let mut entropy_sum = 0f64;
     for count in hist.values() {
         let p = *count as f64 / pixel_count;
